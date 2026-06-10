e65fdc8672b6233eb236bfa21493801794e210ca
Bogdanc2004 <bogdan.comsa.2004@gmail.com>
2026-06-10 23:43:51 +0300

    AWS communication

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from adafruit_ads1x15.analog_in import AnalogIn
 from gpiozero import OutputDevice
 import pickle
 import pandas as pd
+import paho.mqtt.client as mqtt
+import ssl;
+import json
+import os
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ENDPOINT = os.getenv("AWS_ENDPOINT")
+CA_PATH = os.getenv("CA_PATH")
+CERT_PATH = os.getenv("CERT_PATH")
+KEY_PATH = os.getenv("KEY_PATH")
 
 relay = OutputDevice(17, active_high=False, initial_value=False)
 # Initialize I2C and ADS1115
@@ -15,6 +28,18 @@ try:
 except Exception as e:
     print(f"Critical startup error. Soil sensor not found: {e}")
     exit(1)
+
+client = mqtt.Client()
+client.tls_set(ca_certs=CA_PATH, certfile=CERT_PATH, keyfile=KEY_PATH,cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+print("Connecting to AWS IoT Core...")
+try:
+    client.connect(AWS_ENDPOINT, 8883, keepalive=60)
+    print("Connected to AWS IoT Core successfully!")
+except Exception as e:
+    print(f"Error connecting to AWS IoT Core: {e}")
+    exit(1)
+
+client.loop_start()  # Start the MQTT client loop in a separate thread
 
 try:
     # Open the box and load the ML model created by the other script
@@ -54,46 +79,62 @@ def humidity_map(read_value, max_value, min_value):
         return 0
     else:
         return round(percent, 1)
-
-while True:
-    try:
-        raw_value = soil_channel.value
-        temp, hum = read_dht11()
-        soil_hum = humidity_map(raw_value, 17080, 10500)
-        
-        if temp is not None and hum is not None:
-            current_conditions = pd.DataFrame(
-                [[temp, hum, soil_hum]],
-                columns=['Temperature', 'Air_Humidity', 'Soil_Humidity']
-            )
-
-            # Ask the AI what action to take
-            ai_decision = ai_model.predict(current_conditions)[0]
+try:
+    while True:
+        try:
+            raw_value = soil_channel.value
+            temp, hum = read_dht11()
+            soil_hum = humidity_map(raw_value, 17080, 10500)
             
-            if ai_decision == 1:  
-                print("AI decided: soil is dry. Pumping a dose of water...")
-                relay.on()
-                time.sleep(5)  # Pump runs for EXACTLY 5 seconds (adjust based on your pump size)
-                relay.off()    # Force the pump off
+            if temp is not None and hum is not None:
+                current_conditions = pd.DataFrame(
+                    [[temp, hum, soil_hum]],
+                    columns=['Temperature', 'Air_Humidity', 'Soil_Humidity']
+                )
+                payload = {
+                    "Temperature": temp,
+                    "Air_Humidity": hum,
+                    "Soil_Humidity": soil_hum,
+                    "Timestamp": datetime.datetime.now().isoformat()
+                }
+                message = json.dumps(payload)
+                client.publish("test/topic", message, qos=1)
+                time.sleep(60)
+                # Ask the AI what action to take
+                ai_decision = ai_model.predict(current_conditions)[0]
                 
-                print("Watering complete. Waiting 5 minutes for soil to absorb...")
-                time.sleep(300) # Sleep for 5 minutes (300 seconds) so the AI doesn't double-water
-                continue # Skips the standard 3-second sleep below and restarts the loop fresh
-                
-            else:  
-                print("AI decided: soil is moist enough. Pump remains OFF.")
-                # Ensure the relay is off, just in case
-                if relay.is_active:
-                    relay.off()
-        else:
-            # If the DHT11 fails, we need to know about it.
-            print("Waiting for valid DHT11 sensor data...")
+                if ai_decision == 1:  
+                    print("AI decided: soil is dry. Pumping a dose of water...")
+                    relay.on()
+                    time.sleep(5)  # Pump runs for EXACTLY 5 seconds (adjust based on your pump size)
+                    relay.off()    # Force the pump off
+                    payload = {
+                        "Relay": "on",
+                        "Timestamp": datetime.datetime.now().isoformat()
+                    }
+                    message = json.dumps(payload)
+                    client.publish("test/topic", message, qos=1)
+                    print("Watering complete. Waiting 5 minutes for soil to absorb...")
+                    
+                    
+                else:  
+                    print("AI decided: soil is moist enough. Pump remains OFF.")
+                    # Ensure the relay is off, just in case
+                    if relay.is_active:
+                        relay.off()
+            else:
+                # If the DHT11 fails, we need to know about it.
+                print("Waiting for valid DHT11 sensor data...")
 
-    except Exception as e:
-        print(f"Hardware error: {e}. Turning off the pump for safety.")
-        if relay.is_active:
-            relay.off()
-    
-    # Standard heartbeat: check conditions every 3 seconds IF not currently watering
-    time.sleep(3)
+        except Exception as e:
+            print(f"Hardware error: {e}. Turning off the pump for safety.")
+            if relay.is_active:
+                relay.off()
+        
+        # Standard heartbeat: check conditions every 3 seconds IF not currently watering
+        time.sleep(3600)
+except KeyboardInterrupt:
+    print("Program is closed")
+    client.loop_stop()
+    client.disconnect()
         
